6ec2004298d289140bb40769a6590219bfe7ed47
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-06-10 22:15:12 +0700

    fix error saat menghapus data di pengadaan_header

--- delete_pengadaan_header.php
+++ delete_pengadaan_header.php
@@ -13,10 +13,10 @@ forEach ( $kumpulanBarang as $barang) {
 
 $query = "SELECT * FROM tb_barang WHERE kd_barang IN (".(join(",", $kumpulanKodeBarang)).") ";
 $result = mysqli_query($conn, $query);
-$resultData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$resultData = (count($kumpulanKodeBarang) === 0) ? [] : mysqli_fetch_all($result, MYSQLI_ASSOC); // error
 
 mysqli_begin_transaction($conn);
-$kondisi = array_map(function ($val, $val2) {
+$kondisi = (count($kumpulanKodeBarang) === 0) ? [true] : array_map(function ($val, $val2) {
   global $conn;
   $query2 = "UPDATE tb_barang SET stok ='".($val['stok'] - $val2['stok_masuk'])."' WHERE kd_barang = '".($val['kd_barang'])."'";
   $query3 = "DELETE FROM tb_pengadaan_detail WHERE kd_pengadaan='".$val2['kd_pengadaan']."' && kd_barang='".$val2['kd_barang']."'" ;
@@ -25,10 +25,10 @@ $kondisi = array_map(function ($val, $val2) {
 
 if (hapus("tb_pengadaan_header", "kd_pengadaan", $_GET['kode_pengadaan']) && (array_search(false, $kondisi) ? false : true)) {
   mysqli_commit($conn);
-  echo "<script>alert('Data berhasil didelete !!'); window.location = 'tampil_pengadaan_header.php'</script>";
+  echo "<script>alert('Data berhasil didelete !!'); //window.location = 'tampil_pengadaan_header.php'</script>";
 } else {
   mysqli_rollback($conn);
-  echo "<script>alert('Data gagal didelete !!'); window.location = 'tampil_pengadaan_header.php'</script>";
+  echo "<script>alert('Data gagal didelete !!'); //window.location = 'tampil_pengadaan_header.php'</script>";
 }
 
 
